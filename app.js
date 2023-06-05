6b43c898e2c1add56c18fb9806aa35d74357b091
Dave Fancher <dave@davefancher.com>
2023-06-05 02:12:09 -0400

    Styling, behavior updates, and bug fixes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const fs = require("node:fs");
 const http = require("node:http");
+const os = require("node:os");
 const path = require("node:path");
+const { URL } = require("node:url");
 const { Writable } = require("node:stream");
 const { threadId } = require("node:worker_threads");
 
@@ -10,17 +12,64 @@ const dayjs = require("dayjs");
 const express = require("express");
 const { Server: SocketIOServer } = require("socket.io");
 const winston = require("winston");
-const { combine, colorize, label, printf, json, timestamp } = winston.format;
+const { combine, colorize, printf, json, timestamp } = winston.format;
 
 const { LOG_LEVEL, PROCESSING_STRATEGY } = require("./lib/constants");
 const { PIPE, PIPE_SYNC, TEE_SYNC, WITH_TIMING } = require("./lib/extensions");
 
 const TARGET_SCALES = [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 150, 200 ];
+// const TARGET_SCALES = [ 10, 20, 30 ];
+
+let lastCpuTimes = os.cpus();
+
+const getAverageUsage =
+    lastTimes =>
+        os
+            .cpus()
+            .map(
+                (cpu, ix) => {
+                    const lastCpuTime = lastTimes[ix];
+                    const deltas = {
+                        user: cpu.times.user - lastCpuTime.times.user,
+                        sys: cpu.times.sys - lastCpuTime.times.sys,
+                        idle: cpu.times.idle - lastCpuTime.times.idle
+                    };
+                    const percentage = 1 - deltas.idle / (deltas.user + deltas.sys + deltas.idle);
+
+                    return {
+                        model: cpu.model,
+                        times: cpu.times,
+                        percentage
+                    };
+                });
+
+const conditionalLabel =
+    winston.format((info, opts) => {
+        if (Reflect.has(info, "label")) {
+            return info;
+        }
+
+        Reflect.set(info, "label", opts.label);
+
+        return info;
+    });
+
+const formatGeneratedFileNameAsUrl =
+    baseUrl =>
+        generatedFileName =>
+            generatedFileName
+                [PIPE_SYNC](path.parse)
+                [PIPE_SYNC](
+                    p =>
+                        new URL(
+                            `assets/images/generated/${path.basename(p.dir)}/${encodeURI(p.base)}`,
+                            baseUrl)
+                            .toString())
 
 const doResize =
     (socket, strategy) =>
-        fileBuffer => ({
-            sourceFileName: "image.jpg",
+        (fileName, fileBuffer) => ({
+            sourceFileName: fileName,
             inputBuffer: fileBuffer,
             targetDirectory: path.join(__dirname, "output", dayjs().format("YYYYMMDD_HHmmss") + "_" + strategy),
             targetScales: TARGET_SCALES
@@ -34,13 +83,34 @@ const doResize =
             `./lib/strategies/${strategy}`
                 [PIPE_SYNC](require)
                 [WITH_TIMING](`Resize (${strategy})`, "info"))
-        [PIPE](() => socket.emit("resize-complete", strategy))
+        [PIPE](result => {
+            if (!socket.connected) {
+                console.error("Socket disconnected!");
+                return;
+            }
+
+            socket.emit(
+                "resize-complete",
+                {
+                    strategy,
+                    files:
+                        result
+                            .map(({ value }) => value)
+                            .map(formatGeneratedFileNameAsUrl("http://localhost"))
+                });
+        });
 
 const app = express();
 const httpServer = http.createServer(app);
 
 const io =
-    new SocketIOServer(httpServer, { maxHttpBufferSize: 1e8 })
+    new SocketIOServer(
+        httpServer,
+        {
+            maxHttpBufferSize: 1e8,
+            transports: [ "websocket" ],
+            pingTimeout: 60000
+        })
         .on("connection",
             socket =>
                 PROCESSING_STRATEGY
@@ -50,17 +120,6 @@ const io =
                             s.on(`resize-${strategy}`, doResize(s, strategy)),
                             socket));
 
-const conditionalLabel =
-    winston.format((info, opts) => {
-        if (Reflect.has(info, "label")) {
-            return info;
-        }
-
-        Reflect.set(info, "label", opts.label);
-
-        return info;
-    });
-
 winston
     .add(new winston.transports.Console({
         level: LOG_LEVEL.DEBUG,
@@ -68,10 +127,10 @@ winston
             combine(
                 colorize({ all: true }),
                 timestamp(),
-                conditionalLabel({ label: `Thread Id: ${threadId.toString().padStart(2, "0")}`}),
+                conditionalLabel({ label: `[Thread Id: ${threadId.toString().padStart(2, "0")}]`}),
                 printf(
                     ({ level, message, label, timestamp }) =>
-                        `${timestamp} ${level.padEnd(10, " ")} [${label}] ${message}`))
+                        `${timestamp} ${level.padEnd(10, " ")} ${label} ${message}`))
     }))
     // .add(new winston.transports.File({
     //     filename: "./log/resize.log",
@@ -89,7 +148,7 @@ winston
         stream: new Writable({
             write (chunk, encoding, callback) {
                 io.emit("log", chunk.toString());
-                callback()
+                callback();
             }
         }),
         format: combine(
@@ -98,6 +157,20 @@ winston
             json())
     }));
 
+setInterval(
+    () => {
+        lastCpuTimes = getAverageUsage(lastCpuTimes);
+
+        io.emit(
+            "cpu",
+            lastCpuTimes
+                .map(cpu => ({ model: cpu.model, percentage: cpu.percentage })));
+    },
+    1000);
+
+app.use("/assets/images/generated", express.static("./output"))
+app.use("/lib/chartjs", express.static("./node_modules/chart.js/dist"));
+app.use("/lib/revealjs", express.static("./node_modules/reveal.js/dist"));
 app.use("/", express.static("./client/"));
 
 const HTTP_PORT = 80;
---
+++ client/assets/scripts/index.js
@@ -0,0 +1,258 @@
+let logScrollInterval;
+
+const createCpuChart =
+    chartIndex => {
+        const chartContainer = document.createElement("div");
+        const chartCanvas = document.createElement("canvas");
+        chartContainer.appendChild(chartCanvas);
+
+        return new Chart(
+            chartCanvas,
+            {
+                type: "line",
+                data: {
+                    labels: new Array(60).fill("00"),
+                    datasets: [
+                        {
+                            label: `CPU ${chartIndex + 1}`,
+                            data: new Array(60).fill(0)
+                        }
+                    ]
+                },
+                options: {
+                    animation: false,
+                    elements: {
+                        line: {
+                            fill: "stack",
+                            borderWidth: 1,
+                            tension: 0.25,
+                            borderColor: "#002b36",
+                            backgroundColor: "#002b36"
+                        },
+                        point: {
+                            pointStyle: false
+                        }
+                    },
+                    layout: {
+                        autoPadding: false,
+                        padding: 0
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            color: "#002b36",
+                            text: `CPU ${chartIndex + 1}`
+                        }
+                    },
+                    scales: {
+                        y: {
+                            min: 0,
+                            max: 100,
+                            ticks: {
+                                color: "#002b36"
+                            }
+                        },
+                        x: {
+                            display: false
+                        }
+                    }
+                }
+            });
+    };
+
+const updateCpuCharts =
+    (chartContainer, time) => {
+        const cpuUsageCharts = [];
+
+        return data => {
+            const second = time.getSeconds().toString().padStart(2, "0");
+
+            if (chartContainer.children.length === 0) {
+                data.forEach((cpu, ix) => {
+                    const chart = createCpuChart(ix);
+
+                    cpuUsageCharts.push(chart);
+                    chartContainer.append(chart.canvas.parentElement);
+                });
+
+                setTimeout(Reveal.layout, 0);
+            }
+
+            data
+                .forEach((cpu, ix) => {
+                    const usageChartData = cpuUsageCharts[ix]?.data;
+
+                    if (!usageChartData) return;
+
+                    void usageChartData.labels.shift();
+                    usageChartData.labels.push(second);
+
+                    const dataset = usageChartData.datasets[0];
+
+                    void dataset.data.shift();
+                    dataset.data.push(Math.round(cpu.percentage * 100));
+
+                    cpuUsageCharts[ix].update();
+                });
+        };
+    };
+
+const writeLogMessage =
+    logList =>
+        logJson => {
+            const log = JSON.parse(logJson);
+
+            const newLogItem = document.createElement("li");
+            newLogItem.classList.add("log_item", `log_item_${log.level}`);
+            newLogItem.appendChild(
+                document.createTextNode(`${log.timestamp} ${log.label} ${log.level.padEnd(8, "\xa0")} ${log.message}`)
+            );
+
+            logList.appendChild(newLogItem);
+        };
+
+const showGeneratedFiles =
+    (fileList, resizeResult) => {
+        resizeResult
+            .files
+            .map(fileUrl => {
+                const urlParts = new URL(fileUrl).pathname.split("/");
+                const fileName = decodeURI(urlParts[urlParts.length - 1]);
+
+                const link = document.createElement("A");
+                link.href = fileUrl;
+                link.text = fileName;
+                link.target = "_blank";
+
+                const listItem = document.createElement("li");
+                listItem.appendChild(link);
+
+                fileList.appendChild(listItem);
+            });
+    };
+
+// Adapted from https://stackoverflow.com/a/45411081
+const scrollElementToLastChild =
+    logList => {
+        if (!logList.lastChild?.getBoundingClientRect) return;
+
+        const parentRect = logList.getBoundingClientRect();
+        const childRect = logList.lastChild.getBoundingClientRect();
+
+        if (!childRect) return;
+
+        const isViewable = (childRect.top >= parentRect.top) && (childRect.bottom <= parentRect.bottom);
+
+        if(!isViewable) {
+            const scrollTop = childRect.top - parentRect.top;
+            const scrollBottom = childRect.bottom - parentRect.bottom;
+
+            logList.scrollTop +=
+                Math.abs(scrollTop) < Math.abs(scrollBottom)
+                    ? scrollTop
+                    : scrollBottom;
+        }
+    }
+
+const DEMO = {
+    SERIAL: "serial",
+    PARALLEL: "parallel",
+    WORKER_THREAD: "workerThread",
+    THREAD_POOL: "threadPool"
+};
+
+const runDemo =
+    strategy =>
+        socket => {
+                const __fileSelector = document.getElementById(`fileSelector_${strategy}`);
+                const __cpuUsage = document.getElementById(`cpuUsage_${strategy}`);
+                const __logList = document.getElementById(`logList_${strategy}`);
+                const __fileList = document.getElementById(`fileList_${strategy}`);
+
+                if (__fileSelector.files.length < 1) {
+                    alert("You need to select a file first");
+                    return;
+                }
+
+                __cpuUsage.replaceChildren([]);
+                __logList.replaceChildren([]);
+                __fileList.replaceChildren([]);
+
+                socket
+                    .on(
+                        "disconnect",
+                        reason => {
+                            const logMessage = JSON.stringify({
+                                timestamp: new Date().toISOString(),
+                                label: "[Thread Id: N/A]",
+                                level: "error",
+                                message: `Socket disconnected: ${reason}`
+                            });
+
+                            writeLogMessage(__logList)(logMessage);
+                        })
+                    .on(
+                        "log",
+                        writeLogMessage(__logList))
+                    .on(
+                        "cpu",
+                        updateCpuCharts(__cpuUsage, new Date()))
+                    .on(
+                        "resize-complete",
+                        result => {
+                            setTimeout(
+                                () => {
+                                    showGeneratedFiles(__fileList, result);
+
+                                    void socket
+                                        .off("disconnect")
+                                        .off("log")
+                                        .off("cpu")
+                                        .off("resize-complete");
+
+                                    clearInterval(__logInterval);
+                                },
+                                5000);
+                            });
+
+                let __logInterval =
+                    setInterval(
+                        () => scrollElementToLastChild(__logList),
+                        1000);
+
+                const file = __fileSelector.files[0];
+
+                setTimeout(
+                    () => socket.emit(`resize-${strategy}`, file.name, file),
+                    5000);
+            };
+
+const runWorkerThreadDemo = runDemo(DEMO.WORKER_THREAD);
+
+const DEMO_RUNNERS = Object.freeze({
+    [DEMO.SERIAL]: runDemo(DEMO.SERIAL),
+    [DEMO.PARALLEL]: runDemo(DEMO.PARALLEL),
+    [DEMO.WORKER_THREAD]: runDemo(DEMO.WORKER_THREAD),
+    [DEMO.THREAD_POOL]: runDemo(DEMO.THREAD_POOL)
+});
+
+window.addEventListener(
+    "load",
+    () => {
+        Reveal.initialize({
+            disableLayout: true
+        });
+
+        const socket = io("ws://localhost", { transports: [ "websocket" ] });
+
+        Object
+            .values(DEMO)
+            .forEach(strategy => {
+                document
+                    .getElementById(`startButton_${strategy}`)
+                    .addEventListener("mouseup", () => DEMO_RUNNERS[strategy](socket))
+            });
+    });
